9b7909ef3aff106a37e929ee02cc0d68ca596238
Benjamin Memisevic <ben.memisevic.qc@gmail.com>
2023-05-14 14:53:35 +0200

    next problem and tests
    ''

--- src/main.rs
+++ src/main.rs
@@ -15,3 +15,23 @@ pub fn count_odds(low: i32, high: i32) -> i32 {
     }
     (d / 2) + 1
 }
+
+pub fn average(salary: Vec<i32>) -> f64 {
+    let mut min = salary[0];
+    let mut max = 0;
+    let mut sum = 0;
+    for val in salary.iter() {
+        if val > &max {
+            max = *val;
+            sum = sum + val;
+            continue;
+        }
+        if val < &min {
+            min = *val;
+            sum = sum + val;
+            continue;
+        }
+        sum = sum + val;
+    } 
+    ((sum - (min + max)) as f64) / ((salary.len() - 2) as f64)
+}
--- src/test.rs
+++ src/test.rs
@@ -3,3 +3,9 @@ fn test_p1523() {
     assert_eq!(crate::count_odds(1, 3), 2);
     assert_eq!(crate::count_odds(5, 10), 3);
 }
+
+#[test]
+fn test_average() {
+    assert_eq!(crate::average(vec![1, 2, 3]), 2 as f64);
+    assert_eq!(crate::average(vec![1i32, 2, 3, 4, 5]), 3 as f64); 
+}
